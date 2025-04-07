8323d04f9d5e101a9a0f5b0b4ceca5a866611ec5
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-04-07 22:53:52 +0300

    setting up server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function server() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => {
+          return {
+            property: error.property,
+            message: Object.values(error.constraints).join(', '),
+          };
+        });
+        return new BadRequestException({
+          isSuccess: false,
+          statusCode: 400,
+          message: 'Validation Failed',
+          details: messages,
+        });
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+server();
