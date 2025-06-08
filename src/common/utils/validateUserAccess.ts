e47ef29b12dfad2eb04a14772162fdabc797405d
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-08 21:01:22 +0300

    update user role access checker

--- src/common/utils/validateUserAccess.ts
+++
@@ -1,8 +0,0 @@
-import { ForbiddenException } from '@nestjs/common';
-import { ALLOWED_AUTHENTICATED_ROLES } from '../constants/roles.constants';
-
-export const validateUserAccess = (requestingUser: any): void => {
-  if (!ALLOWED_AUTHENTICATED_ROLES.includes(requestingUser.role)) {
-    throw new ForbiddenException();
-  }
-};
