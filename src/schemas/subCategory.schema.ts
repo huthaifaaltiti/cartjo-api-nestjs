e08d35b44b457644b94d398254aa984b5a3d2f59
huthaifa.titi <haltiti@arabica.org>
2025-06-18 10:55:38 +0300

    adding subCategory schema

---
+++ src/schemas/subCategory.schema.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type SubCategoryDocument = SubCategory & Document;
+
+class Name {
+  @Prop({ required: true }) ar: string;
+  @Prop({ required: true }) en: string;
+}
+
+@Schema({ collection: 'subCategories', timestamps: true })
+export class SubCategory extends Document {
+  @Prop({ required: true })
+  name: Name;
+
+  @Prop({ required: true, default: null })
+  image?: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  })
+  categoryId: mongoose.Types.ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null })
+  createdBy: mongoose.Types.ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null })
+  deletedBy: mongoose.Types.ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null })
+  unDeletedBy: mongoose.Types.ObjectId;
+
+  @Prop({ default: Date.now })
+  createdAt?: Date;
+}
+
+export const SubCategorySchema = SchemaFactory.createForClass(SubCategory);
