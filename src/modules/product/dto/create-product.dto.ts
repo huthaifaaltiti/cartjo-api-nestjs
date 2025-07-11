14fc1f79760e448a3c48b419cc48f2dca75a039a
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-07-11 20:20:19 +0300

    update types

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { Type, Transform } from 'class-transformer';
 import {
   IsEnum,
   IsMongoId,
@@ -12,7 +13,6 @@ import {
 
 import { Currency } from 'src/enums/currency.enum';
 import { TypeHint } from 'src/enums/typeHint.enums';
-
 import { Locale } from 'src/types/Locale';
 
 export class CreateProductDto {
@@ -54,10 +54,29 @@ export class CreateProductDto {
   currency: Currency;
 
   @IsOptional()
-  @IsNumber()
+  @Type(() => Number)
+  @Transform(({ value }) => {
+    console.log('discountRate',{ value });
+    if (value === null || value === undefined || value === '') return undefined;
+    const num = Number(value);
+    return isNaN(num) ? value : num;
+  })
+  @IsNumber({}, { message: 'discountRate must be a number' })
   @Min(1, { message: 'Price discount rate cant be less than 1' })
   @Max(100, { message: 'Price discount rate cant be more than 100' })
   discountRate?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @Transform(({ value }) => {
+    console.log('availableCount',{ value });
+    if (value === null || value === undefined || value === '') return undefined;
+    const num = Number(value);
+    return isNaN(num) ? value : num;
+  })
+  @IsNumber({}, { message: 'availableCount must be a number' })
+  @Min(1, { message: 'available count cant be less than 1' })
+  availableCount?: number;
 
   @IsNotEmpty()
   @IsString()
