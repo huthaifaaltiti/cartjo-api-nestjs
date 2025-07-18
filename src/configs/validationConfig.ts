677f3df87c594b2cf861b484bfb7831156a24b95
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-07-18 11:03:56 +0300

    feat(validation): add max length validation for category names

---
+++ src/configs/validationConfig.ts
@@ -0,0 +1,6 @@
+export const validationConfig = {
+  category: {
+    nameMinChars: 3,
+    nameMaxChars: 50,
+  },
+};
--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,15 +1,29 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 import { Locale } from 'src/types/Locale';
+import { validationConfig } from 'src/configs/validationConfig';
+
+const { nameMinChars, nameMaxChars } = validationConfig.category;
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty({ message: 'Category ar name is required' })
-  @MinLength(5, { message: 'Category ar name cannot be empty' })
+  @MinLength(nameMinChars, {
+    message: `Category ar name cannot be less than (${nameMinChars}) characters`,
+  })
+  @MaxLength(nameMaxChars, {
+    message: `Category ar name cannot be more than (${nameMaxChars}) characters`,
+  })
   name_ar: string;
 
   @IsString()
   @IsNotEmpty({ message: 'Category en name is required' })
-  @MinLength(5, { message: 'Category en name cannot be empty' })
+  @MinLength(nameMinChars, {
+    message: `Category en name cannot be less than (${nameMinChars}) characters`,
+  })
+  @MaxLength(nameMaxChars, {
+    message: `Category en name cannot be more than (${nameMaxChars}) characters`,
+  })
   name_en: string;
 
   @IsString()
