0c2ebd0b9327d85734b8a826a165ba4d68a97bc5
huthaifa.titi <haltiti@arabica.org>
2025-12-12 10:55:45 +0300

    log host name

--- src/pipes/logging.pipe.ts
+++ src/pipes/logging.pipe.ts
@@ -3,6 +3,7 @@ import { Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class LoggingPipe implements PipeTransform {
   transform(value: any) {
+    console.log('process.env.HOST_NAME: ', process.env.HOST_NAME)
     console.log('🔥 Incoming request body BEFORE validation:', value);
     return value;
   }
