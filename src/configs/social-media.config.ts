1d9c911a99afb64804b6ebd59081fd344afb3016
huthaifa.titi <haltiti@arabica.org>
2025-12-20 12:12:59 +0300

    fix social media links

--- src/configs/social-media.config.ts
+++ src/configs/social-media.config.ts
@@ -12,24 +12,11 @@ export const getSocialMediaIconsPaths = (hostUrl: string) => ({
 
 export const getSocialMediaLinks = () => ({
   whatsappNumber: process.env.SOCIAL_MEDIA_WHATSAPP_NUMBER,
-  whatsappIconPath: `${hostUrl}/public/assets/icons/whatsapp.png`,
   whatsappLink: process.env.SOCIAL_MEDIA_WHATSAPP_LINK,
-  
-  xIconPath: `${hostUrl}/public/assets/icons/x.png`,
   xLink: process.env.SOCIAL_MEDIA_X_LINK,
-
-  facebookIconPath: `${hostUrl}/public/assets/icons/facebook.png`,
   facebookLink: process.env.SOCIAL_MEDIA_FACEBOOK_LINK,
-
-  instagramIconPath: `${hostUrl}/public/assets/icons/instagram.png`,
   instagramLink: process.env.SOCIAL_MEDIA_INSTAGRAM_LINK,
-
-  snapchatIconPath: `${hostUrl}/public/assets/icons/snapchat.png`,
   snapchatLink: process.env.SOCIAL_MEDIA_SNAPCHAT_LINK,
-
-  linkedinIconPath: `${hostUrl}/public/assets/icons/linkedin.png`,
   linkedInLink: process.env.SOCIAL_MEDIA_LINKEDIN_LINK,
-
-  tiktokIconPath: `${hostUrl}/public/assets/icons/tiktok.png`,
   tiktokLink: process.env.SOCIAL_MEDIA_TIKTOK_LINK,
 });
