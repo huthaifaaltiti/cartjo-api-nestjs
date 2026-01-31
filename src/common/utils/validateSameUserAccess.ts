583f80040447e73b9a43dc952bb0604a9a9c6fa7
huthaifa.titi <haltiti@arabica.org>
2026-01-31 13:54:32 +0300

    fix validators

--- src/common/utils/validateSameUserAccess.ts
+++ src/common/utils/validateSameUserAccess.ts
@@ -6,10 +6,12 @@ export const validateSameUserAccess = (
   requestingUserId: string,
   targetUserId: string,
   lang: Locale,
-): void => {
+): void | boolean => {
   const isSameUser = requestingUserId === targetUserId;
 
   if (!isSameUser) {
     throw new ForbiddenException(getMessage('authorization_noAccess', lang));
   }
+
+  return true;
 };
