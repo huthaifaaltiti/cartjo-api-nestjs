6eca8d1aa61e747676f426bb411c9bcf1aa72a81
huthaifa.titi <haltiti@arabica.org>
2026-01-31 22:02:12 +0300

    refactor user role access validator

--- src/common/utils/validateUserRoleAccess.ts
+++ src/common/utils/validateUserRoleAccess.ts
@@ -1,20 +1,28 @@
 import { ForbiddenException } from '@nestjs/common';
-
 import { ALLOWED_AUTHENTICATED_ROLES } from '../constants/roles.constants';
 import { getMessage } from './translator';
+import { UserRole } from 'src/enums/user-role.enum';
+
+type AccessLevel = 'user' | 'admin';
 
 /**
- * Ensures that the logged-in and authenticated user is performing an action.
- * @param requestingUser - The user being acted upon.
- * @param lang - Language for error messages.
+ * Validates whether the requesting user has access based on access level.
+ * - default: admin
+ * - user: USER only
  */
 export const validateUserRoleAccess = (
   requestingUser: any,
   lang: 'en' | 'ar',
-): void | boolean => {
+  accessLevel: AccessLevel = 'admin',
+): true => {
   const { role } = requestingUser;
 
-  if (ALLOWED_AUTHENTICATED_ROLES.includes(role)) {
+  const roleMap: Record<AccessLevel, UserRole[]> = {
+    user: [UserRole.USER],
+    admin: ALLOWED_AUTHENTICATED_ROLES,
+  };
+
+  if (!roleMap[accessLevel].includes(role)) {
     throw new ForbiddenException(getMessage('authorization_noAccess', lang));
   }
 
