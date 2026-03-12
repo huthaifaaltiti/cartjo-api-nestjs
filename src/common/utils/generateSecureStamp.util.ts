8ad25cda4da3af301fb7da6529656b45b12feace
huthaifa.titi <haltiti@arabica.org>
2026-03-12 23:02:20 +0300

    refactor: function to include 24 secure-chars

--- src/common/utils/generateSecureStamp.util.ts
+++ src/common/utils/generateSecureStamp.util.ts
@@ -1,7 +1,9 @@
 export function generateSecureStamp(): string {
   return (
+    crypto.randomUUID().substring(2, 8) +
     Math.random().toString(36).substring(2, 7) +
-    Date.now().toString() +
-    Math.random().toString(36).substring(2)
+    Date.now().toString().slice(-5) +
+    Math.random().toString(36).substring(2, 4) +
+    crypto.randomUUID().substring(2, 8)
   );
 }
