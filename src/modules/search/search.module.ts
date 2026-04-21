a812525ca2d6c3d46669b239d894023ca3946a46
huthaifa.titi <haltiti@arabica.org>
2026-04-21 21:28:35 +0300

    refactor: search products

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -4,12 +4,17 @@ import { SearchController } from './search.controller';
 import { Product, ProductSchema } from 'src/schemas/product.schema';
 import { WishList, WishListSchema } from 'src/schemas/wishList.schema';
 import { SearchService } from './search.service';
+import {
+  TypeHintConfig,
+  TypeHintConfigSchema,
+} from 'src/schemas/typeHintConfig.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Product.name, schema: ProductSchema },
       { name: WishList.name, schema: WishListSchema },
+      { name: TypeHintConfig.name, schema: TypeHintConfigSchema },
     ]),
   ],
   controllers: [SearchController],
