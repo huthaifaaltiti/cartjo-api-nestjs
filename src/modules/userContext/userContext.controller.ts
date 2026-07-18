56955b334f3d23f4aafb958f88d152f9344b1bd1
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2026-07-18 12:19:50 +0300

    refactor(core): link user context with user permission

--- src/modules/userContext/userContext.controller.ts
+++ src/modules/userContext/userContext.controller.ts
@@ -3,14 +3,18 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserContextService } from './userContext.service';
 import { GetUserContextQuery } from './dto/get-user-context.dto';
 import { ApiPaths } from '../../common/constants/api-paths';
+import { RequirePermissions } from '../../common/decorators/permissions.decorator';
+import { Permission } from '../../enums/permission.enum';
+import { PermissionsGuard } from '../../common/guards/permissions.guard';
 
 @Controller(ApiPaths.UserContext.Root)
 export class UserContextController {
   constructor(private readonly userContextService: UserContextService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @RequirePermissions(Permission.USERS_READ, Permission.PROFILE_READ)
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
   @Get(ApiPaths.UserContext.Get)
-  async getUsers(@Request() req: any, @Query() query: GetUserContextQuery) {
+  async getUserContext(@Request() req: any, @Query() query: GetUserContextQuery) {
     return this.userContextService.getUserContext({
       user: req.user,
       query,
